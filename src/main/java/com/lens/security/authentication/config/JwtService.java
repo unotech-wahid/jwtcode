d69f3e3c8f65a4b4e59a2dde00505b92edeca3f7
shaikh vasim <svasimahmed283@gmail.com>
2024-05-27 20:25:31 +0530

    code fix

--- src/main/java/com/lens/security/authentication/config/JwtService.java
+++ src/main/java/com/lens/security/authentication/config/JwtService.java
@@ -44,7 +44,6 @@ public class JwtService {
 	}
 
 	public String extractUsername(String token) {
-		System.out.println("PPPPPPPPPPPPPPPPPPPPPPPP");
 		return extractClaim(token, Claims::getSubject);
 	}
 
